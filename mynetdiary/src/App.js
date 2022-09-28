6272b948b21e6642441d68fe236307e3a72ba4ec
Adarsh-nihal <adarsh474747@gmail.com>
2022-09-28 21:29:05 +0530

    added

--- mynetdiary/src/App.js
+++ mynetdiary/src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
 import AllRoutes from './Pages/AllRoutes';
 import Navbar from './Components/Navbar';
-import Body from './Pages/Body';
+
+
 function App() {
   return (
     <div className="App">
  
    <Navbar/>
    <AllRoutes/>
-   <Body />
+  
     </div>
   );
 }
--- mynetdiary/src/Pages/Home.jsx
+++ mynetdiary/src/Pages/Home.jsx
@@ -1,10 +1,11 @@
 
-
+import Body from "./Body"
 
 function Home(){
 
     return(
       <>
+      <Body />
       </>
     )
 }
